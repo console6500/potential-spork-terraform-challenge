0ec190db84f49345c69a6cba1f8f0aad5778f2a1
Console 6500 <73613222+console6500@users.noreply.github.com>
2023-05-12 16:52:43 -0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "tf-state-storage-a1e1e6a446badf3f0570560f1e81be127a6a7d75Info"             # the bucket
+    bucket = "tf-state-storage-a1e1e6a446badf3f0570560f1e81be127a6a7d75"             # the bucket
     region = "us-east-2"             # the region
   }
 }
